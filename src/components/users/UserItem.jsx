fe67caec9fc9c2e01cebe44cfc07c6760876f017
Han Hoang <katiehoang268@gmail.com>
2023-03-06 23:19:02 -0600

    update pages and components

---
+++ src/components/users/UserItem.jsx
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+// we want user login name and avatar url
+function UserItem({user}) {
+  return (
+    <div className='card shadow-md compact side bg-base-100'>
+      <div className="card-body flex-row items-center space-x-4">
+        <div>
+          <div className="avatar">
+            <div className="rounded-full shadow w-14 h-14">
+              <img src={user.avatar_url} alt="Profile" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <h2 className="card-title">{user.login}</h2>
+          <Link 
+          className='text-base-content text-opacity-70' 
+          to={`/users/${user.login}`}
+          >
+            Visit Profile
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+}
+
+export default UserItem
